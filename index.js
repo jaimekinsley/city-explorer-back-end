25085cbad9857127c98f4daed2292dabdea2e853
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-06 11:00:38 -0700

    initial set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const app = express ();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ hello: 'world'});
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
